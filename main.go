61f92ad40641db92029a1d98de057264fd4112f0
MM Williams <mmwilliamscodes@gmail.com>
2020-08-19 18:28:31 -0400

    Initializing Repository

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	// cards := newDeck()
+
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+
+	// greeting := "Hi there!"
+	// fmt.Println([]byte(greeting))
+
+	// cards := newDeck()
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("my_cards")
+
+	// cards := newDeckFromFile("my_cards")
+	// cards.print()
+
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
+}
